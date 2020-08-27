3f0079b1ffa5f5c4bc7f0f81ed40083ad12826ce
PravinTiwariER <pravintiwari1892@gmail.com>
2020-08-27 11:34:47 -0700

    Jaxb marshal and unmarshal example

---
+++ jaxb-marshal-two/src/main/java/com/jaxb/marshal/XmlToObject.java
@@ -0,0 +1,25 @@
+package com.jaxb.marshal;
+
+import java.io.File;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+public class XmlToObject {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("question.xml");
+		JAXBContext jaxbContext = JAXBContext.newInstance(Question.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		Question que = (Question) jaxbUnmarshaller.unmarshal(file);
+		System.out.println(que.getId() + " " + que.getQuestionname());
+		System.out.println("Answers:");
+		List<Answer> list=que.getAnswers();  
+        for(Answer ans:list)  {
+        	 System.out.println(ans.getId()+" "+ans.getAnswername()+"  "+ans.getPostedby());  
+        }
+
+	}
+
+}
