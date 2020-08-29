02cc345e1291bfb7ee7821313f87b66e70d7cb9c
PravinTiwariER <pravintiwari1892@gmail.com>
2020-08-30 00:09:54 +0530

    sax-parser two example

---
+++ sax-parser-two/src/main/java/com/saxparser/SAXParserDemo.java
@@ -0,0 +1,23 @@
+package com.saxparser;
+
+import java.io.File;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class SAXParserDemo {
+
+	public static void main(String[] args) {
+		try {
+	         File inputFile = new File("input.xml");
+	         SAXParserFactory factory = SAXParserFactory.newInstance();
+	         SAXParser saxParser = factory.newSAXParser();
+	         UserHandler userhandler = new UserHandler();
+	         saxParser.parse(inputFile, userhandler);     
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+
+	}
+
+}
